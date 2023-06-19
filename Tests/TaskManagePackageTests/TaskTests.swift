9f812e3adabacbc948da46c5f727d5536c870c95
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-06-19 10:21:09 +0600

    created subject under test, and modified my code

--- Tests/TaskManagePackageTests/TaskTests.swift
+++ Tests/TaskManagePackageTests/TaskTests.swift
@@ -4,25 +4,23 @@ import XCTest
 
 final class TaskTests: XCTestCase {
 
-	var task: Task!
-
-	override func setUp() {
-		super.setUp()
-		task = Task(title: "Test Task")
-	}
-
-	override func tearDown() {
-		task = nil
-		super.tearDown()
-	}
-
 	func test_init_isCompleteShouldBeFalse() {
-		XCTAssertEqual(task.title, "Test Task")
-		XCTAssertFalse(task.completed, "Не верно создается класс")
+		let sut = makeSUT()
+
+		XCTAssertEqual(sut.title, "Test Task")
+		XCTAssertFalse(sut.completed, "Не верно создается класс")
 	}
 
 	func test_completedSetTrue_completedShouldBeTrue() {
-		task.completed = true
-		XCTAssertTrue(task.completed, "Не возможно завершить задание")
+		let sut = makeSUT()
+
+		sut.completed = true
+		XCTAssertTrue(sut.completed, "Не возможно завершить задание")
+	}
+}
+
+private extension TaskTests {
+	func makeSUT() -> Task {
+		return Task(title: "Test Task")
 	}
 }
